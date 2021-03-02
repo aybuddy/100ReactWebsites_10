3b40dc14a169fdd9ff1f0d6b8e1e80b4c9b556b1
Aaron Yu <ayybuddyy@gmail.com>
2021-03-02 17:17:58 -0500

    Add in Home component and banner image

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Header from './Header';
+import Home from './Home';
 
 function App() {
   return (
@@ -9,15 +10,18 @@ function App() {
       <div className='app'>
         <Switch>
           <Route path='/checkout'>
+            <Header />
             <h1>Checkout</h1>
           </Route>
+
           <Route path='/login'>
             <h1>Log In</h1>
           </Route>
+
           {/* Last Route path is the default path */}
           <Route path='/'>
             <Header />
-            <h1>Home Page</h1>
+            <Home />
           </Route>
         </Switch>
       </div>
