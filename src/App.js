5bf67d81c94dcc467e3c86da757c4cf2287a91ce
Aaron Yu <ayybuddyy@gmail.com>
2021-03-02 15:55:34 -0500

    Add react-router-dom and set up routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className='App'>
-      <h1>Hello World</h1>
-    </div>
+    <Router>
+      <div className='app'>
+        <Switch>
+          <Route path='/checkout'>
+            <h1>Checkout</h1>
+          </Route>
+          <Route path='/login'>
+            <h1>Log In</h1>
+          </Route>
+          {/* Last Route path is the default path */}
+          <Route path='/'>
+            <h1>Home Page</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyBbVxZnKCBjUglLT0cQUDwzXyd4V_Z1Ydk',
+  authDomain: 'clone-cp-be216.firebaseapp.com',
+  projectId: 'clone-cp-be216',
+  storageBucket: 'clone-cp-be216.appspot.com',
+  messagingSenderId: '22252743347',
+  appId: '1:22252743347:web:93de1ef7633a4938f9dec6',
+  measurementId: 'G-R47CSGKLYV',
+};
